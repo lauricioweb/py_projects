664130865103ad2b4433052aa439442bdb6b2f70
lauricioweb <lauriciomalek@gmail.com>
2024-09-18 23:10:13 -0300

    variables 10/10

---
+++ input_function.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome");
+print("\nO seu nome é: " + nome);
+
+nota1 = float(input("digite a nota 1"));
+nota2 = float(input("digite a nota 2"));
+
+media = (nota1 + nota2) / 3;
+
+print(media)
