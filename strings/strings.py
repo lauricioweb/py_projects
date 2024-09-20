4fdee3c7853e9518ea946356be2a009b636d5120
lauricioweb <lauriciomalek@gmail.com>
2024-09-19 21:58:33 -0300

    strings 4/4

--- strings/strings.py
+++ strings/strings.py
@@ -45,13 +45,39 @@ frase = "    mior programador lauricio      "
 print("remove espaços em branco de uma frase")
 
 print(frase.strip())
+# o strip ira remover da string o caractere informado por parametro
+#ex => frase.strip(*)  ira remover todos os "*" da frase
 
 
 
 # transformando frase em array separando por espaço em branco;
 texto = "este é apenas um texto de exemplo"
-texto = texto.split()
+texto = texto.split() 
 print(texto)
+
+
+
+# transformando array em frase separadas por espaço em branco
+
+textarr = ["ola", "sou", "apenas", "um", "texto"]
+textstr = " ".join(textarr)
+print(textstr)
+
+
+# para inserir variaveis dentro de uma string use "f" antes das aspas da seguinte forma
+# f"meu nome é {nome}"
+
+
+nome = "lauricio"
+idade = 33
+altura = 1.50
+
+mensagem = f"Olá meu nome é {nome}. e tenho {idade} de idade e minha altura é {altura:.2f} metros"
+
+print(mensagem)
+
+
+
 
 
 
