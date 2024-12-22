247067c4009de18b869ff3d0cb237d2d047b767f
lauricioweb <lauriciomalek@gmail.com>
2024-12-22 10:46:18 -0300

    exercices strings 2

---
+++ exercices_strings_methods/exercicios-strings-2.py
@@ -0,0 +1,10 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+for numero in lista:
+  if numero % 2 == 0:
+   print(numero)
+   print("este numero é par")
+
+  else:
+   print(numero)
+   print("este numero é impar")
