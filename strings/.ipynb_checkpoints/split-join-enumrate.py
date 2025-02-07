41a4c5f5f030439ac57c71ec110a9b8de7378f0f
Lauricio <developerwebmasterpro@gmail.com>
2025-02-07 15:55:40 -0300

    using tuples

---
+++ strings/.ipynb_checkpoints/split-join-enumrate.py
@@ -0,0 +1,10 @@
+string = "teste de digitacao, este texto e apenas um teste"
+
+list = string.split(",")
+
+#print(list)
+
+frutas = ["manana", "batata", "uva"]
+
+result = frutas.sort()
+print(result)
